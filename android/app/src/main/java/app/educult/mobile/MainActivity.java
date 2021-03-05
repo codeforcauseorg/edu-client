d736a6871b4a4299b9c6617d44d4e09f534f9abc
KeenWarrior <anujgargcse@gmail.com>
2021-03-06 00:35:07 +0530

    Setup native login with capacitor firebase

---
+++ android/app/src/main/java/app/educult/mobile/MainActivity.java
@@ -0,0 +1,24 @@
+package app.educult.mobile;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+      add(CapacitorFirebaseAuth.class);
+    }});
+  }
+}
