b833725fbfbbe8e3a5006b658d72fd8bf1880824
rajat <rajat2411gupta@gmail.com>
2021-03-12 10:07:45 +0530

    Revert "Adding padding to container"
    This reverts commit 61844b2f96d34b244047bbedeeb427d40590c2ce.

--- src/pages/wishListView/index.js
+++ src/pages/wishListView/index.js
@@ -1,20 +1,18 @@
+import { List, ListItem, makeStyles } from '@material-ui/core'
 import React from 'react'
 import WishlistCard from '../../components/wishlistCard'
-import { List, ListItem, makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
-  ul: {
-    padding: "0 0 3rem 0"
-  },
+  root: {},
   li: {
-    padding: '1rem 0px'
+    padding: '14px 0px'
   }
 }))
 
 const WishListView = () => {
   const classes = useStyles();
 
-  return <List className={classes.ul}>
+  return <List>
     {[1, 2, 3].map((item, index) => {
       return <ListItem key={index} className={classes.li} >
         <WishlistCard />
@@ -24,4 +22,4 @@ const WishListView = () => {
   </List >
 }
 
-export default WishListView;
+export default WishListView 
