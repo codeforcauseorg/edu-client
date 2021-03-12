edb6919462d97df52fe4d9e69f242ca8587c1fb4
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-12 18:07:42 +0530

    fix: eslint errors

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ export const APP_VERSION = "2.0.0";
 export const API_BASE_URL = "/api";
 export const ENABLE_REDUX_LOGGER = false;
 
-let config = { APP_VERSION, API_BASE_URL, ENABLE_REDUX_LOGGER };
+const config = { APP_VERSION, API_BASE_URL, ENABLE_REDUX_LOGGER };
 
 export default config;
