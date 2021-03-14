c08cc630755abcb683bc8387d4cece6e41dd0c7d
Rohan Maisuria <54482189+RohankumarM@users.noreply.github.com>
2021-03-14 09:11:45 +0530

    Updated Bottom Navigation (#78)
    * updated bottom navigation
    
    * updated bottom navigation
    
    * updated bottom navigation
    
    * updated bottom navigation

--- src/layouts/MainLayout/BottomNav.js
+++ src/layouts/MainLayout/BottomNav.js
@@ -9,6 +9,24 @@ import {
   AddCircleOutlineOutlined,
 } from "@material-ui/icons";
 import { useHistory, useLocation } from "react-router";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import AppBar from "@material-ui/core/AppBar";
+import Fade from "@material-ui/core/Fade";
+import "./BottomNav.css";
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Fade appear={true} direction="down" in={!trigger}>
+      {children}
+    </Fade>
+  );
+}
 
 const useStyles = makeStyles({
   root: {
@@ -19,28 +37,48 @@ const useStyles = makeStyles({
   },
 });
 
-export default function BottomNav() {
+export default function BottomNav(props) {
   const classes = useStyles();
 
   const history = useHistory();
   const location = useLocation();
   const [value, setValue] = React.useState(location.pathname);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    history.push(newValue);
-  };
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  //   history.push(newValue);
+  // };
 
   return (
-    <BottomNavigation value={value} onChange={handleChange} showLabels className={classes.root}>
-      <BottomNavigationAction label="Home" value="/home" icon={<HomeOutlined />} />
-      <BottomNavigationAction label="My Classes" value="/personal" icon={<PlayCircleOutline />} />
-      <BottomNavigationAction
-        label="Wishlist"
-        value="/wishlist"
-        icon={<AddCircleOutlineOutlined />}
-      />
-      <BottomNavigationAction label="Profile" value="/profile" icon={<AccountCircleOutlined />} />
-    </BottomNavigation>
+    <HideOnScroll {...props}>
+      <AppBar className="cubic">
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            history.push(newValue);
+          }}
+          showLabels
+          className={classes.root}
+        >
+          <BottomNavigationAction label="Home" value="/home" icon={<HomeOutlined />} />
+          <BottomNavigationAction
+            label="My Classes"
+            value="/personal"
+            icon={<PlayCircleOutline />}
+          />
+          <BottomNavigationAction
+            label="Wishlist"
+            value="/wishlist"
+            icon={<AddCircleOutlineOutlined />}
+          />
+          <BottomNavigationAction
+            label="Profile"
+            value="/profile"
+            icon={<AccountCircleOutlined />}
+          />
+        </BottomNavigation>
+      </AppBar>
+    </HideOnScroll>
   );
 }
