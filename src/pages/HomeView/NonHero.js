806b8e6bbb57157af4f337bffa4e067c46439c8e
Devesh <rockdev1234560@gmail.com>
2021-03-22 22:37:32 +0530

    added the backtotop button component to pages

--- src/pages/HomeView/NonHero.js
+++ src/pages/HomeView/NonHero.js
@@ -1,4 +1,4 @@
-import { Container, makeStyles, Typography } from "@material-ui/core";
+import { Container, makeStyles, List, ListItem, Typography } from "@material-ui/core";
 import React from "react";
 import CourseCard from "../../components/courseCard/courseCard1";
 
@@ -12,14 +12,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function NonHero() {
+export default function NonHero(props) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Container>
         <Typography variant="h3">Continue Learning</Typography>
-        <CourseCard />
+
+        <List>
+          {[1, 2, 3, 4, 5, 6, 7].map((item, index) => {
+            return (
+              <ListItem key={index} className={classes.li}>
+                <CourseCard />
+              </ListItem>
+            );
+          })}
+        </List>
       </Container>
     </div>
   );
