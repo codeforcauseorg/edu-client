81259387cfdad8571972048f06dac6e853a9d4a1
Rajat Gupta <55191777+rajatgupta24@users.noreply.github.com>
2021-03-23 21:01:42 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import SwipeableRoutes from "react-swipeable-routes";
 
 import MainLayout from "./layouts/MainLayout";
@@ -15,22 +15,24 @@ import Terms from "./pages/Info/Terms";
 
 const renderRoutes = () => (
   <React.Fragment>
-    <MainLayout>
+     <Switch>
       <Route path="/" exact>
         <Redirect to="/home" />
       </Route>
       <Route path="/course/:id" exact component={CourseDetail} />
-      <SwipeableRoutes replace>
-        <Route path="/home" exact component={HomePage} />
-        <Route path="/personal" exact component={CoursePage} />
-        <Route path="/wishlist" exact component={WishlistPage} />
-        <Route path="/profile" exact component={ProfilePage} />
-      </SwipeableRoutes>
-    </MainLayout>
-    <DocsLayout>
-      <Route path="/privacy" exact component={Privacy} />
-      <Route path="/terms-and-conditions" exact component={Terms} />
-    </DocsLayout>
+      <MainLayout>
+        <SwipeableRoutes replace>
+          <Route path="/home" exact component={HomePage} />
+          <Route path="/personal" exact component={CoursePage} />
+          <Route path="/wishlist" exact component={WishlistPage} />
+          <Route path="/profile" exact component={ProfilePage} />
+        </SwipeableRoutes>
+      </MainLayout>
+      <DocsLayout>
+        <Route path="/privacy" exact component={Privacy} />
+        <Route path="/terms-and-conditions" exact component={Terms} />
+      </DocsLayout>
+    </Switch>
   </React.Fragment>
 );
 
