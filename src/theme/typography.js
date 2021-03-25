8693d812aebf1d2054fee359959501ba655a7219
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-26 01:19:42 +0530

    fix: typography

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -4,49 +4,59 @@ export default {
 
   h1: {
     fontWeight: 700,
-    fontSize: 35,
+    fontSize: 50,
     letterSpacing: "-0.24px",
   },
   h2: {
-    fontWeight: 600,
-    fontSize: 20,
+    fontWeight: 700,
+    fontSize: 40,
     letterSpacing: "-0.24px",
   },
   h3: {
-    fontWeight: 700,
-    fontSize: 12,
+    fontWeight: 500,
+    fontSize: 32,
     letterSpacing: "-0.06px",
   },
   h4: {
-    fontWeight: 600,
+    fontWeight: 500,
     fontSize: 20,
     letterSpacing: "-0.06px",
   },
   h5: {
     fontWeight: 600,
-    fontSize: 16,
+    fontSize: 18,
     letterSpacing: "-0.05px",
   },
   h6: {
     fontWeight: 700,
-    fontSize: 10,
+    fontSize: 12,
+    letterSpacing: "-0.05px",
+  },
+  subtitle1: {
+    fontWeight: 400,
+    fontSize: 18,
+    letterSpacing: "-0.05px",
+  },
+  subtitle2: {
+    fontWeight: 500,
+    fontSize: 16,
     letterSpacing: "-0.05px",
   },
   body1: {
     fontWeight: 400,
-    fontSize: 10,
+    fontSize: 18,
     letterSpacing: "-0.05px",
   },
   body2: {
     fontWeight: 400,
-    fontSize: 14,
+    fontSize: 16,
     letterSpacing: "-0.05px",
   },
   overline: {
     fontWeight: 500,
   },
   caption: {
-    fontSize: 8,
+    fontSize: 12,
     fontWeight: 500,
   },
 };
