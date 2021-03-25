8a3058fa675b9040917fc753d9418e6835ab4552
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-03-26 00:06:24 +0530

    Set view to mobile and auto detect eslint react

--- src/components/Button/ButtonComponent.js
+++ src/components/Button/ButtonComponent.js
@@ -10,9 +10,7 @@ const useStyles = makeStyles((theme) => ({
     textTransform: "capitalize",
     boxShadow: "0px 10px 40px rgba(0, 0, 0, 0.25)",
     padding: "10px",
-    [theme.breakpoints.down("sm")]: {
-      width: "100%",
-    },
+    width: "100%",
     "&:hover": {
       backgroundColor: "rgba(166, 0, 0, 0.8)",
     },
