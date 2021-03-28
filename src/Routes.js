5fd019b3e63b98e21582330ac1893c1be7ebb3a6
poojadas07 <poojadas04kv@gmail.com>
2021-03-28 07:34:49 +0530

    Peer discussion page design

--- src/Routes.js
+++ src/Routes.js
@@ -14,8 +14,6 @@ import Privacy from "./pages/Info/Privacy";
 import Terms from "./pages/Info/Terms";
 import Faqs from "./pages/Info/Faqs";
 import About from "./pages/Info/About";
-import Checkout from "./pages/CheckoutView";
-import MentorRating from "./pages/MentorRating";
 import PeerPage from "./pages/PeerDiscussionPage/peer";
 import StudentDashboard from "./pages/StudentDashboardPage";
 import AssignmentSubmission from "./pages/StudentDashboardPage/AssignmentSubmission/AssignmentSubmission";
