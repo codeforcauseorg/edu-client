5bba2f9b832da0cabb21798e17823f0bb0d32608
Abhijay007j <Abhijay007j@gmail.com>
2021-03-30 14:39:39 +0530

    Add Go- back and scroll to top functionality ot course description page

--- src/pages/CourseDetail.js
+++ src/pages/CourseDetail.js
@@ -10,6 +10,10 @@ import Schedule from "../components/courseDetails/schedule";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { Typography } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import Fab from "@material-ui/core/Fab";
+import ScrollTop from "../components/backTop/index";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 const useStyles = makeStyles((theme) => ({
   navigation: {
@@ -36,6 +40,12 @@ const useStyles = makeStyles((theme) => ({
   svg: {
     color: "white",
   },
+  backtotop: {
+    minWidth: "100%",
+    display: "flex",
+    justifyContent: "center",
+    paddingBottom: "24px",
+  },
 }));
 
 const heroData = courseDetail[0].heroSection;
@@ -43,16 +53,18 @@ const descriptionData = courseDetail[0].descriptionSection;
 const perksData = courseDetail[0].perksSection;
 const coursesData = courseDetail[0].coursesSection;
 
-function CourseDetail() {
+function CourseDetail(props) {
   const classes = useStyles();
+  const history = useHistory();
   return (
     <>
+      <div id="back-to-top-anchor"></div>
+      <ScrollToTop />
       <div className={classes.navigation}>
-        <ArrowBackIcon className={classes.svg} />
+        <ArrowBackIcon className={classes.svg} onClick={() => history.goBack()} />
         <MoreVertIcon className={classes.svg} />
       </div>
       <div className={classes.courseDetail}>
-        <ScrollToTop />
         <Hero
           title={heroData.title}
           description={heroData.description}
@@ -107,6 +119,13 @@ function CourseDetail() {
             />
           ))}
         </CardContainer>
+        <div className={classes.backtotop}>
+          <ScrollTop {...props}>
+            <Fab color="secondary" size="small" aria-label="scroll back to top">
+              <KeyboardArrowUpIcon />
+            </Fab>
+          </ScrollTop>
+        </div>
       </div>
     </>
   );
