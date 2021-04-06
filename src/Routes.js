e349c5c27e70406fc723761c554f8aea0c73d2a9
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-04-06 13:06:48 +0530

    Added: Job opening page

--- src/Routes.js
+++ src/Routes.js
@@ -19,6 +19,7 @@ import MentorRating from "./pages/MentorRating";
 import PeerPage from "./pages/PeerDiscussionPage/peer";
 import StudentDashboard from "./pages/StudentDashboardPage";
 import AssignmentSubmission from "./pages/StudentDashboardPage/AssignmentSubmission/AssignmentSubmission";
+import JobOpenings from "./pages/JobOpeningPage";
 
 const renderRoutes = () => (
   <Container maxWidth="xs" style={{ margin: "auto", padding: 0 }}>
@@ -36,6 +37,7 @@ const renderRoutes = () => (
       <Route path="/terms-and-conditions" exact component={Terms} />
       <Route path="/dashboard" exact component={StudentDashboard} />
       <Route path="/dashboard/assignment/:id" exact component={AssignmentSubmission} />
+      <Route path="/job-openings" exact component={JobOpenings} />
       <MainLayout>
         <SwipeableRoutes replace>
           <Route path="/home" exact component={HomePage} />
--- src/pages/StudentDashboardPage/index.js
+++ src/pages/StudentDashboardPage/index.js
@@ -1,11 +1,21 @@
-import { AppBar, Container, IconButton, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Container,
+  Fade,
+  IconButton,
+  makeStyles,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import DashboardHeader from "./DashboardComponents/DashboardHeader";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ProgressCourse from "./DashboardComponents/ProgressCourse";
 import CourseAssignments from "./DashboardComponents/CourseAssignments";
-
+import { useHistory } from "react-router";
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: 0,
@@ -21,25 +31,53 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 function StudentDashboard() {
+  const history = useHistory();
   const classes = useStyles();
+  const [anchor, setAnchor] = React.useState(null);
+  const open = Boolean(anchor);
+
+  const handleClick = (event) => {
+    setAnchor(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchor(null);
+  };
   return (
     <Container component="main" maxWidth="xs" className={classes.root}>
-      <AppBar position="static" className={classes.appBar}>
+      <AppBar position="sticky" className={classes.appBar}>
         <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.backButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.backButton}
+            color="inherit"
+            onClick={() => history.goBack()}
+          >
             <ArrowBackIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" className={classes.title}>
             Dashboard
           </Typography>
-          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MoreVertIcon />
+          <IconButton edge="end" className={classes.menuButton} color="inherit">
+            <MoreVertIcon onClick={handleClick} />
           </IconButton>
         </Toolbar>
       </AppBar>
       <DashboardHeader />
       <ProgressCourse />
       <CourseAssignments />
+      <Menu
+        id="fade-menu"
+        anchorEl={anchor}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={() => history.push("/job-openings")}>
+          <Typography>Jobs Openings</Typography>
+        </MenuItem>
+      </Menu>
     </Container>
   );
 }
