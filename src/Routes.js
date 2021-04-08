b88bcbbb3c3916d4c98103a1bb03d41365b2f4dc
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-08 20:11:58 +0530

    added assets

--- src/Routes.js
+++ src/Routes.js
@@ -20,6 +20,7 @@ import PeerPage from "./pages/PeerDiscussionPage/peer";
 import StudentDashboard from "./pages/StudentDashboardPage";
 import AssignmentSubmission from "./pages/StudentDashboardPage/AssignmentSubmission/AssignmentSubmission";
 import JobOpenings from "./pages/JobOpeningPage";
+import Statistics from "./pages/Statistics";
 
 const renderRoutes = () => (
   <Container maxWidth="xs" style={{ margin: "auto", padding: 0 }}>
@@ -39,6 +40,7 @@ const renderRoutes = () => (
       <Route path="/dashboard" exact component={StudentDashboard} />
       <Route path="/dashboard/assignment/:id" exact component={AssignmentSubmission} />
       <Route path="/job-openings" exact component={JobOpenings} />
+      <Route path="/statistics" exact component={Statistics} />
       <MainLayout>
         <SwipeableRoutes replace>
           <Route path="/faq" exact component={Faqs} />
