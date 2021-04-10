6de160f20e474c28b597e6d79716dc0dc3330ce7
poojadas07 <poojadas04kv@gmail.com>
2021-04-10 08:19:09 +0530

    Header style changes

--- src/pages/wishListView/index.js
+++ src/pages/wishListView/index.js
@@ -1,10 +1,12 @@
-import { List, ListItem, makeStyles } from "@material-ui/core";
-
+import { List, ListItem, makeStyles, AppBar, IconButton, Toolbar, Typography,} from "@material-ui/core";
 import ScrollToTop from "../../utils/ScrollToTop";
 import WishlistCard from "../../components/wishlistCard";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import Fab from "@material-ui/core/Fab";
 import ScrollTop from "../../components/backTop/index";
+import { useHistory } from "react-router-dom";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -16,12 +18,35 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
   },
+  backButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    background: "#160050",
+  },
 }));
 
 const WishListView = (props) => {
   const classes = useStyles();
+  const history = useHistory();
   return (
     <>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.backButton} color="inherit" aria-label="menu">
+            <ArrowBackIcon onClick={() => history.goBack()} />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            WishList
+          </Typography>
+          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MoreVertIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <div id="back-to-top-anchor"></div>
       <ScrollToTop />
       <List>
