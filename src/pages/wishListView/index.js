2024e5b39acf461aa80934660703e92d35455751
poojadas07 <poojadas04kv@gmail.com>
2021-04-10 08:22:00 +0530

    Header UI changes with back button

--- src/pages/wishListView/index.js
+++ src/pages/wishListView/index.js
@@ -1,4 +1,12 @@
-import { List, ListItem, makeStyles, AppBar, IconButton, Toolbar, Typography,} from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  makeStyles,
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import ScrollToTop from "../../utils/ScrollToTop";
 import WishlistCard from "../../components/wishlistCard";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
