6f1f7139f0c18e4e99f17d4097a529625095d74c
poojadas07 <poojadas04kv@gmail.com>
2021-04-10 08:19:09 +0530

    Header style changes

--- src/pages/CoursePageViewWithVideo/index.js
+++ src/pages/CoursePageViewWithVideo/index.js
@@ -12,6 +12,8 @@ import CourseCard from "../../components/courseCard/courseCard1";
 import ScrollTop from "../../components/backTop/index";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import Fab from "@material-ui/core/Fab";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: { borderRadius: "10px", color: "#000" },
@@ -41,9 +43,20 @@ export default function MyCourses(props) {
     <>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            className={classes.backButton}
+            color="inherit"
+            onClick={() => history.goBack()}
+          >
+            <ArrowBackIcon />
+          </IconButton>
           <Typography variant="h6" color="inherit" className={classes.title}>
             Your Courses
           </Typography>
+          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MoreVertIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Container className={classes.root}>
