328c4f4497ec513ae4af202911c98fbe1d755c68
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-05-31 00:44:21 +0530

    feat: add delay in mock instance

--- src/utils/mock.js
+++ src/utils/mock.js
@@ -1,6 +1,6 @@
 import AxiosMockAdapter from "axios-mock-adapter";
 import axios from "./mockaxios";
 
-const instance = new AxiosMockAdapter(axios, { delayResponse: 0 });
+const instance = new AxiosMockAdapter(axios, { delayResponse: 3000 });
 
 export default instance;
