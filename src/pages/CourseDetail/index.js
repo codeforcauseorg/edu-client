2435c6333d0e1ec44463346fe91a4f191c9c2950
Rajat Gupta <rajat2411gupta@gmail.com>
2021-06-04 00:52:23 +0530

    CourseDetail-desktop-body

--- src/pages/CourseDetail/index.js
+++ src/pages/CourseDetail/index.js
@@ -1,57 +1,18 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Description from "../../components/courseDetails/description";
 import Hero from "../../components/courseDetails/Hero";
-import CardContainer from "../../components/cardContainer/cardContainer";
-import SimpleCard from "../../components/simpleCard/simpleCard";
 import { makeStyles } from "@material-ui/core/styles";
 import courseDetail from "../../data/courseDetail.json";
-import Perks from "../../components/courseDetails/perks";
-import Schedule from "../../components/courseDetails/schedule";
+import Details from "../../components/courseDetails/CourseDetailBody";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import { Typography } from "@material-ui/core";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 const useStyles = makeStyles((theme) => ({
-  navigation: {
-    widht: "100%",
-    height: "100px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "8px 10px",
-    backgroundColor: "white" /* For browsers that do not support gradients */,
-    backgroundImage: "linear-gradient(#4C0098, white)",
-  },
-  courseDetail: {
-    padding: "0 15px",
-    textAlign: "start",
-  },
-  title: {
-    fontWeight: "700",
-    margin: "10px",
-  },
-  subTitle: {
-    margin: "0 10px",
-    padding: "0 15px",
-  },
-  svg: {
-    color: "white",
-    cursor: "pointer",
-  },
-  backtotop: {
-    minWidth: "100%",
-    display: "flex",
-    justifyContent: "center",
-    paddingBottom: "24px",
-  },
 }));
 
 const heroData = courseDetail[0].heroSection;
 const descriptionData = courseDetail[0].descriptionSection;
-const perksData = courseDetail[0].perksSection;
-const coursesData = courseDetail[0].coursesSection;
 
 function CourseDetail(props) {
   const classes = useStyles();
@@ -94,49 +55,8 @@ function CourseDetail(props) {
           videoUrl={heroData.videoUrl}
           price={heroData.price}
         />
-        <Description description={descriptionData.description} mentors={descriptionData.mentors} />
-        <Perks
-          perks={perksData.perks}
-          afterCourse={perksData.afterCourse}
-          prerequisites={perksData.prerequisites}
-        />
-        <Schedule />
-        <Typography className={classes.title} variant="subtitle2" color="primary">
-          Beginner Courses
-        </Typography>
+        <Details description={descriptionData.description} mentors={descriptionData.mentors}/>
       </div>
-      <Typography className={classes.subTitle} variant="h2" component="h2">
-        Learn something New
-      </Typography>
-      <CardContainer>
-        {coursesData.map((course, index) => (
-          <SimpleCard
-            key={index}
-            image={course.image}
-            title={course.title}
-            subTitle={course.subTitle}
-            detail={course.detail}
-            price={course.price}
-            showIcon={true}
-          />
-        ))}
-      </CardContainer>
-      <Typography className={classes.subTitle} variant="h2" component="h2">
-        Top Picks
-      </Typography>
-      <CardContainer>
-        {coursesData.map((course, index) => (
-          <SimpleCard
-            key={index}
-            image={course.image}
-            title={course.title}
-            subTitle={course.subTitle}
-            detail={course.detail}
-            price={course.price}
-            showIcon={false}
-          />
-        ))}
-      </CardContainer>
     </>
   );
 }
