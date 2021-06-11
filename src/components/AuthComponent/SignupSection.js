7f44c9cef55cf2a5b726918c9019c2a731b95bc9
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-06-11 16:08:17 +0530

    feat: add signup section

---
+++ src/components/AuthComponent/SignupSection.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+function SignupSection() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Box my={18}>
+        <Typography variant="subtitle2" color="textPrimary" className={classes.subtitle}>
+          Start Learning
+        </Typography>
+        <Typography color="textPrimary" className={classes.title}>
+          Create an account
+        </Typography>
+        <TextField fullWidth margin="normal" label="Email" variant="outlined" />
+        <TextField fullWidth margin="normal" label="Password" variant="outlined" />
+        <Button fullWidth className={classes.signupButton}>
+          <Typography variant="body2">Sign up</Typography>
+        </Button>
+        <Button fullWidth className={classes.loginButton}>
+          <img className={classes.googleIcon} src="assets/icon/social icons/google_icon.svg" />
+          <Typography variant="body2"> Sign up with google</Typography>
+        </Button>
+      </Box>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontSize: "1.8rem",
+    fontWeight: 800,
+    cursor: "pointer",
+  },
+  subtitle: {
+    textTransform: "uppercase",
+  },
+  signupButton: {
+    background: theme.palette.primary.main,
+    color: "#fff",
+    textTransform: "none",
+    "&:hover": {
+      background: theme.palette.primary.main,
+    },
+    padding: theme.spacing(2, 0),
+    marginTop: theme.spacing(3),
+  },
+  loginButton: {
+    color: theme.palette.primary.main,
+    textTransform: "none",
+    padding: theme.spacing(1.5, 0),
+    marginTop: theme.spacing(2),
+    boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.25)",
+  },
+  googleIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export default SignupSection;
