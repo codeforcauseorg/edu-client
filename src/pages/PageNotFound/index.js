7cf11ea5b5866efaf82c66196d1d8af684960449
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-06-20 19:05:07 +0530

    chore: add page not found screen

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,16 +1,78 @@
-import { makeStyles, Box } from "@material-ui/core";
+import { makeStyles, Box, Typography, Chip, Button } from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router";
 
 function PageNotFound() {
   const classes = useStyles();
+  const history = useHistory();
 
-  return <Box className={classes.root}></Box>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.imageContainer}>
+        <img className={classes.image} src="illustrations/page404.svg" alt="" />
+      </Box>
+      <Box className={classes.infoContainer}>
+        <Box className={classes.chipContainer}>
+          <Chip className={classes.chip} label="Page not found" />
+        </Box>
+        <Typography className={classes.errorText} color="textPrimary">
+          Oh NO! Error 404
+        </Typography>
+        <Typography variant="h2" color="textSecondary">
+          Mybe bigfoot has broken this page.Come back to the homepage
+        </Typography>
+        <Box className={classes.buttonContainer}>
+          <Button className={classes.button} onClick={() => history.push("/home")}>
+            Back to Homepage
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
-    background: "#000",
+    background: "#fff",
+    display: "flex",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+  imageContainer: {
+    width: "100%",
+  },
+  infoContainer: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    marginRight: theme.spacing(10),
+  },
+  chip: {
+    background: "#2B3E4C",
+    color: "#fff",
+  },
+  chipContainer: {
+    paddingBottom: theme.spacing(2),
+  },
+  button: {
+    background: "#587BDA",
+    color: "#fff",
+    textTransform: "none",
+    padding: theme.spacing(1.5),
+    borderRadius: "10px",
+    "&:hover": {
+      background: "#587BDA",
+    },
+  },
+  errorText: {
+    fontSize: "50px",
+    fontWeight: 800,
+  },
+  buttonContainer: {
+    paddingTop: theme.spacing(4),
   },
 }));
 
