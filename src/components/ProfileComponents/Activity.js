ee7873564e2d998f9d9a53e51597ba78a2c4c13b
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-06-26 21:41:25 +0530

    chore: add activity cards

--- src/components/ProfileComponents/Activity.js
+++ src/components/ProfileComponents/Activity.js
@@ -1,7 +1,64 @@
+import { makeStyles, Box, Grid } from "@material-ui/core";
+import ActivityCard from "./SubComponents/ActivityCard";
 import React from "react";
 
+const activityCardElements = [
+  {
+    title: "Doubt Forum",
+    icon: "assets/icon/ActivityIcons/Chat.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+  },
+  {
+    title: "Contests and Statistics ",
+    icon: "assets/icon/ActivityIcons/Chart.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+  },
+  {
+    title: "Classroom",
+    icon: "assets/icon/ActivityIcons/Profile.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+  },
+  {
+    title: "Gamification Board",
+    icon: "assets/icon/ActivityIcons/Ticket Star.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+  },
+  {
+    title: "Referal & Coupon Code",
+    icon: "assets/icon/ActivityIcons/Discount.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+  },
+  {
+    title: "Interview your friends",
+    icon: "assets/icon/ActivityIcons/user_square.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+  },
+];
+
 function Activity() {
-  return <div></div>;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Grid container spacing={4} justify="center">
+        {activityCardElements.map((items, index) => (
+          <ActivityCard key={index} props={items} />
+        ))}
+      </Grid>
+    </Box>
+  );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 export default Activity;
