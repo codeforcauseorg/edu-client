8cf3ff6361bb709346361b600c971711c728b907
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-06-26 13:44:22 +0530

    profile section cleanup

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,50 +1,25 @@
-import { Box, Button, makeStyles } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { logout } from "../../actions/accountActions";
-import FooterComponent from "./ProfileComponent/FooterComponent";
-import ScoreBoardComponent from "./ProfileComponent/ScoreBoardComponent";
-import ListComponent from "./ProfileComponent/ListComponent";
-import HeaderComponent from "./ProfileComponent/HeaderComponent";
+import ProfileHero from "../../components/ProfileComponents/ProfileHero";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: 0,
-    overflow: "hidden",
-    "&::-webkit-scrollbar": {
-      display: "none",
-    },
-  },
-  buttonwrapper: {
-    display: "flex",
-    flexDirection: "column",
-    margin: "10px 30px 0px 30px",
-    padding: "8px",
-  },
-  button: {
-    color: "#9B0000",
-    fontWeight: 600,
-    textTransform: "none",
-    border: "2px solid rgb(155,0,0,0.7)",
-    borderRadius: "10px",
-  },
-}));
 function ProfilePage() {
-  const dispatch = useDispatch();
   const classes = useStyles();
   return (
-    <>
-      <HeaderComponent />
-      <ScoreBoardComponent />
-      <ListComponent />
-      <Box className={classes.buttonwrapper}>
-        <Button className={classes.button} variant="outlined" onClick={() => dispatch(logout)}>
-          Log out
-        </Button>
-      </Box>
-      <FooterComponent />
-    </>
+    <Container className={classes.root}>
+      <ProfileHero />
+      <Typography className={classes.title} variant="h2" color="textPrimary">
+        Profile & settings
+      </Typography>
+    </Container>
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: "90%",
+  },
+  title: {
+    marginTop: theme.spacing(8),
+  },
+}));
 export default ProfilePage;
