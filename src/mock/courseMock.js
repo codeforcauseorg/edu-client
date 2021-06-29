389b0a43a27394050aebbb211a9c0468420689eb
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-06-29 09:02:43 +0530

    chore: add mock data course endpoints

--- src/mock/courseMock.js
+++ src/mock/courseMock.js
@@ -1,5 +1,8 @@
-// import moment from "moment";
 import mock from "../utils/mock";
-// import course from "./course";
+import popular from "./courses/popular/index";
+import upcoming from "./courses/upcoming/index";
+import explore from "./courses/explore/index";
+import courseDetail from "./courses/courseDetail/index";
 
-mock.onGet("/api/courses/:courseid").reply(200, {});
+mock.onGet("/api/courses/all").reply(200, { popular, upcoming, explore });
+mock.onGet("/api/courses/:courseid").reply(200, courseDetail);
