ab523944115f742fb2410b7ee2397a0714a0ff32
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-06-29 12:16:18 +0530

    remove: loading state action & reducers

--- src/store/actions/courseActions.js
+++ src/store/actions/courseActions.js
@@ -1,15 +1,8 @@
 // Action types
-export const FETCH_COURSE_REQUEST = "@course/fetchCourseRequest";
 export const FETCH_COURSE_SUCCESS = "@course/fetchCourseSuccess";
 export const FETCH_COURSE_FAILURE = "@course/fetchCoursefailure";
 
 // Action creators
-
-export const fetchCourseRequest = () => {
-  return {
-    type: FETCH_COURSE_REQUEST,
-  };
-};
 
 export const fetchCourseSuccess = (course) => {
   return {
--- src/store/reducers/courseReducers.js
+++ src/store/reducers/courseReducers.js
@@ -1,13 +1,8 @@
 import { produce } from "immer";
-import {
-  FETCH_COURSE_SUCCESS,
-  FETCH_COURSE_REQUEST,
-  FETCH_COURSE_FAILURE,
-} from "../actions/courseActions";
+import { FETCH_COURSE_SUCCESS, FETCH_COURSE_FAILURE } from "../actions/courseActions";
 
 const initialState = {
-  loading: false,
-  course: [],
+  course: null,
   error: "",
 };
 
@@ -15,19 +10,11 @@ export default function courseReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_COURSE_SUCCESS:
       return produce(state, (draft) => {
-        draft.loading = false;
         draft.course = action.payload;
-        draft.error = "";
-      });
-    case FETCH_COURSE_REQUEST:
-      return produce(state, (draft) => {
-        draft.loading = true;
-        draft.course = [];
         draft.error = "";
       });
     case FETCH_COURSE_FAILURE:
       return produce(state, (draft) => {
-        draft.loading = false;
         draft.course = [];
         draft.error = action.payload;
       });
