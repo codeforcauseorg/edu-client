0bc4f6e9e3986a21bbf26b8de856ac1e2f863521
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-07-04 02:58:52 +0530

    chore: add doubt forum section

--- src/components/DashboardComponents/MainSection/DoubtsSection.js
+++ src/components/DashboardComponents/MainSection/DoubtsSection.js
@@ -1,7 +1,56 @@
+import { Box, Button, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
+import EditIcon from "@material-ui/icons/Edit";
 
 function DoubtsSection() {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.headerContainer}>
+        <Box className={classes.titleSection}>
+          <Typography variant="h4" className={classes.title} gutterBottom>
+            Forum
+          </Typography>
+          <Typography variant="h5" className={classes.subtitle}>
+            Questions on forum
+          </Typography>
+        </Box>
+        <Box>
+          <Button className={classes.button}>
+            <Typography variant="h6" className={classes.buttonLabel}>
+              Ask A Question
+            </Typography>
+            <EditIcon />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "#F5F8FB",
+    height: 150,
+    borderRadius: "5px",
+    padding: theme.spacing(4),
+  },
+  headerContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    textTransform: "uppercase",
+  },
+  subtitle: {
+    textTransform: "uppercase",
+  },
+  buttonLabel: {
+    marginRight: theme.spacing(2),
+  },
+  button: {
+    color: theme.palette.primary.main,
+  },
+}));
 export default DoubtsSection;
