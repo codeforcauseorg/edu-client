408fb79203f16ce52cc1019e0e29b4e45cd243a2
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-07-12 19:22:45 +0530

    chore: add course filter in search section

--- src/pages/SearchSection/index.js
+++ src/pages/SearchSection/index.js
@@ -1,18 +1,39 @@
-import { Box, makeStyles, Typography, Container, Paper } from "@material-ui/core";
-import React from "react";
-import { useLocation } from "react-router";
+import {
+  Box,
+  makeStyles,
+  Typography,
+  Container,
+  Paper,
+  FormControl,
+  NativeSelect,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router";
 import NavBar from "../../components/NavBar";
 import CourseList from "../../components/SearchComponents/CourseList";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
-function SearchSection() {
+function SearchSection(props) {
   const classes = useStyles();
   const location = useLocation();
+  const history = useHistory();
 
   function useQuery() {
     return new URLSearchParams(location.search);
   }
 
   const query = useQuery().get("q");
+  const sort = useQuery().get("sort");
+  console.log(sort);
+  const [state, setState] = useState("all");
+
+  const handleChange = (event) => {
+    setState(event.target.value);
+    history.push(`/search?q=${query}&sort=${state}`);
+    console.log(event.target.value);
+  };
+
+  const filterList = ["Most Relevant", "Most Reviewed", "Highest Rated", "Newest"];
 
   return (
     <>
@@ -28,6 +49,16 @@ function SearchSection() {
             <Typography variant="h5" className={classes.title}>
               Filter by
             </Typography>
+            <FormControl className={classes.formControl}>
+              <FilterListIcon className={classes.filterIcon} />
+              <NativeSelect className={classes.dropdown} value={state} onChange={handleChange}>
+                {filterList.map((items, index) => (
+                  <option key={index} value={items}>
+                    {items}
+                  </option>
+                ))}
+              </NativeSelect>
+            </FormControl>
           </Container>
         </Paper>
         <Container className={classes.courseListContainer}>
@@ -42,6 +73,7 @@ function SearchSection() {
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    position: "relative",
     maxWidth: "85%",
   },
   courseListContainer: {
@@ -62,8 +94,39 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
+    paddingBottom: theme.spacing(6),
     color: "#fff",
+  },
+  dropdown: {
+    margin: theme.spacing(1),
+    textTransform: "none",
+    padding: theme.spacing(1),
+    borderRadius: "5px",
+    "&.MuiInput-underline:before": {
+      display: "none",
+    },
+    "&.MuiInput-underline:after": {
+      display: "none",
+    },
+  },
+  filterIcon: {
+    color: "#333",
+    marginLeft: theme.spacing(2),
+  },
+  formControl: {
+    width: "100%",
+    boxShadow: "0px 6px 14px -6px rgba(24, 39, 75, 0.12), 0px 10px 32px -4px rgba(24, 39, 75, 0.1)",
+    top: 65,
+    position: "absolute",
+    flexDirection: "row",
+    alignItems: "center",
+    background: "#fff",
+    margin: 8,
+    borderRadius: "5px",
+    height: 50,
+    [theme.breakpoints.down("sm")]: {
+      width: "85%",
+    },
   },
 }));
 
