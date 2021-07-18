e11e5f2510a0fbc0348d0ad3410ba3e75190607d
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-07-19 01:53:28 +0530

    refactor: leaderboard stats card

--- src/components/GamificationComponent/StatsCardSection.js
+++ src/components/GamificationComponent/StatsCardSection.js
@@ -1,12 +1,50 @@
-import { Box, Card, makeStyles } from "@material-ui/core";
+import { Avatar, Box, Card, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
+
+const cardElement = [
+  {
+    title: "Rank",
+    icon: "",
+    subtitle: "Leaderboard Position",
+    value: "05",
+  },
+  {
+    title: "Score",
+    icon: "",
+    subtitle: "Reward Points",
+    value: "300",
+  },
+  {
+    title: "Certificates",
+    icon: "",
+    subtitle: "Course Achevement",
+    value: "04",
+  },
+  {
+    title: "Total Learnings",
+    icon: "",
+    subtitle: "Duration",
+    value: "30 min",
+  },
+];
 
 function StatsCardSection() {
   const classes = useStyles();
   return (
     <Box className={classes.cardContainer}>
-      {[1, 2, 3].map((item, index) => (
-        <Card className={classes.mediaCard} key={index} />
+      {cardElement.map((item, index) => (
+        <Card className={classes.mediaCard} key={index}>
+          <Box className={classes.headerTitle}>
+            <Avatar className={classes.avatar}></Avatar>
+            <Typography variant="h6">{item.title}</Typography>
+          </Box>
+          <Box className={classes.container}>
+            <Typography variant="h1">{item.value}</Typography>
+            <Typography variant="h5" className={classes.subtitle}>
+              {item.subtitle}
+            </Typography>
+          </Box>
+        </Card>
       ))}
     </Box>
   );
@@ -22,10 +60,24 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(4),
   },
   mediaCard: {
-    height: 150,
     width: 300,
     boxShadow: "0px 6px 12px -6px rgba(24, 39, 75, 0.12), 0px 8px 24px -4px rgba(24, 39, 75, 0.08)",
     marginRight: theme.spacing(4),
+  },
+  avatar: {
+    background: "#8496E8",
+    marginRight: theme.spacing(2),
+  },
+  headerTitle: {
+    display: "flex",
+    alignItems: "center",
+    margin: theme.spacing(2),
+  },
+  container: {
+    margin: theme.spacing(2),
+  },
+  subtitle: {
+    marginTop: theme.spacing(1),
   },
 }));
 
