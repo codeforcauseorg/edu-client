23e35022cb38532b7b71beb76a1791ea3baed222
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-07-19 01:52:59 +0530

    refactor: leaderboard section

--- src/components/GamificationComponent/LeaderboardSection.js
+++ src/components/GamificationComponent/LeaderboardSection.js
@@ -1,84 +1,33 @@
-import {
-  Avatar,
-  Box,
-  makeStyles,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@material-ui/core";
-import { withStyles } from "@material-ui/styles";
-import React from "react";
+import { Box } from "@material-ui/core";
+import React, { useState } from "react";
+import leaderboardElement from "../../data/leaderboardRowElements.json";
+import LeaderBoardList from "./LeaderBoardList";
+import Pagination from "./Pagination";
 
 function LeaderboardSection() {
-  const classes = useStyles();
+  const [currentPage, setcurrentPage] = useState(1);
+  const [listsPerPage] = useState(3);
+
+  // Get current list
+  const indexOfLastList = currentPage * listsPerPage;
+  const indexOfFirstList = indexOfLastList - listsPerPage;
+  const currentList = leaderboardElement.slice(indexOfFirstList, indexOfLastList);
+
+  // Change Page
+  const paginate = (pageNumber) => {
+    setcurrentPage(pageNumber);
+  };
+
   return (
-    <TableContainer component={Paper} className={classes.tableContainer}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell align="right">
-              <Box className={classes.score}>Score</Box>
-            </StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {[1, 2, 3, 4, 5].map((index) => (
-            <StyledTableRow key={index}>
-              <StyledTableCell component="th" scope="row">
-                <Box className={classes.infoContainer}>
-                  <Avatar className={classes.avatar} />
-                  Adarsh Kumar Singh
-                </Box>
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                <Box className={classes.score}>200</Box>
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box>
+      <LeaderBoardList leaderboardElement={currentList} />
+      <Pagination
+        listsPerPage={listsPerPage}
+        totalList={leaderboardElement.length}
+        paginate={paginate}
+      />
+    </Box>
   );
 }
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: "#fff",
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:hover": {
-      boxShadow:
-        "0px 6px 12px -6px rgba(24, 39, 75, 0.12), 0px 8px 24px -4px rgba(24, 39, 75, 0.08)",
-      backgroundColor: "#F5F7FA",
-    },
-  },
-}))(TableRow);
-
-const useStyles = makeStyles((theme) => ({
-  tableContainer: {
-    boxShadow: "0px 6px 12px -6px rgba(24, 39, 75, 0.12), 0px 8px 24px -4px rgba(24, 39, 75, 0.08)",
-    marginBottom: theme.spacing(4),
-  },
-  infoContainer: {
-    display: "flex",
-    alignItems: "center",
-  },
-  avatar: {
-    marginRight: theme.spacing(2),
-  },
-  score: {
-    marginRight: theme.spacing(2),
-  },
-}));
 
 export default LeaderboardSection;
