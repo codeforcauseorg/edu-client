7fbbc9fa68aaf62c920e4c0a37e61a0a8d6cf9ed
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-08-01 02:54:00 +0530

    Resolved Cropped text issue in Gamification Board

--- src/components/GamificationComponent/StatsCardSection.js
+++ src/components/GamificationComponent/StatsCardSection.js
@@ -58,15 +58,16 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     marginTop: theme.spacing(4),
     marginBottom: theme.spacing(4),
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down("755")]: {
       flexWrap: "wrap",
     },
   },
   mediaCard: {
     width: 300,
+    minWidth: 165,
     boxShadow: "0px 6px 12px -6px rgba(24, 39, 75, 0.12), 0px 8px 24px -4px rgba(24, 39, 75, 0.08)",
     marginRight: theme.spacing(4),
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down("755")]: {
       marginRight: theme.spacing(0),
       width: "100%",
       marginBottom: theme.spacing(2),
