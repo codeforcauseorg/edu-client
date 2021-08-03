739fd0c9a1530c7b454276ae03c8b024d830e7ee
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-08-04 00:26:04 +0530

    refactor: media card component

--- src/components/CourseMediaCard/MediaCard.js
+++ src/components/CourseMediaCard/MediaCard.js
@@ -68,7 +68,6 @@ function MediaCard({ props, isDeleteButton, onClick }) {
       <CardActionArea>
         <CardContent className={classes.cardContent} onClick={() => history.push(`/course/${_id}`)}>
           <Box className={classes.tagSection}>
-            <Chip variant="outlined" size="small" className={classes.tag} label={tags} />
             <Chip
               size="small"
               className={classes.price}
@@ -81,6 +80,17 @@ function MediaCard({ props, isDeleteButton, onClick }) {
             </Typography>
             <Box className={classes.description}>
               <Typography variant="subtitle2">{courseShortDescription}</Typography>
+            </Box>
+            <Box mt={2} flexWrap="wrap">
+              {tags.map((items, index) => (
+                <Chip
+                  key={index}
+                  variant="outlined"
+                  size="small"
+                  className={classes.tag}
+                  label={items}
+                />
+              ))}
             </Box>
           </Box>
         </CardContent>
@@ -144,6 +154,7 @@ const useStyles = makeStyles((theme) => ({
   },
   tagSection: {
     display: "flex",
+    flexWrap: "wrap",
   },
   ratings: {
     display: "flex",
@@ -161,6 +172,7 @@ const useStyles = makeStyles((theme) => ({
     border: " 1px solid #3740A1",
     borderRadius: "5px",
     background: "#fff",
+    marginBottom: theme.spacing(0.5),
     marginRight: theme.spacing(2),
     color: theme.palette.primary.main,
   },
