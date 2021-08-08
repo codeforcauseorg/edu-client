b91b0c9961787133985c90dd04e2388e433757fe
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-08-08 16:52:02 +0530

    chore(Home section): add responsive support to banner section

--- src/components/HomeViewComponents/Banner.js
+++ src/components/HomeViewComponents/Banner.js
@@ -48,7 +48,7 @@ function Banner() {
       <Container className={classes.infoContainer}>
         <Grid container justify="center" spacing={4}>
           {cardItem.map((items, index) => (
-            <Grid key={index} item xs={12} sm={6} md={6} lg={2}>
+            <Grid key={index} item xs={6} sm={4} md={4} lg={4} xl={2}>
               <Box mt={4}>
                 <items.icon className={classes.icon} />
                 <Typography variant="h5" className={classes.cardTitle}>
@@ -67,14 +67,25 @@ const useStyles = makeStyles((theme) => ({
   root: {
     height: "100%",
     background: "#5848EA",
+    [theme.breakpoints.down("md")]: {
+      padding: theme.spacing(0, 2),
+    },
   },
   title: {
     color: "#fff",
     paddingTop: theme.spacing(4),
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
   },
   subtitle: {
     color: "#fff",
     paddingTop: theme.spacing(2),
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+      paddingTop: theme.spacing(3),
+      fontWeight: 400,
+    },
   },
   container: {
     display: "flex",
@@ -91,6 +102,9 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "85%",
     marginTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
+    [theme.breakpoints.down("md")]: {
+      maxWidth: "100%",
+    },
   },
   cardTitle: {
     marginTop: theme.spacing(2),
