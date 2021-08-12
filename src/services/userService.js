83cdba4ca3350c947557f6bdb01b6fbcdee5d4b3
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2021-08-12 17:06:44 +0530

    chore: add uploadUserDetail + upload coverPhoto  async function

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,11 @@
-import { USER_CART_ENDPOINT, USER_WISHLIST_ENDPOINT } from "../constants/apiEndpoints";
+import {
+  UPDATE_USER_ENDPOINT,
+  USER_CART_ENDPOINT,
+  USER_WISHLIST_ENDPOINT,
+} from "../constants/apiEndpoints";
 import axios from "../utils/axios";
 import errorHandler from "./errorHandler";
+import authService from "./authService";
 
 // Add wishlist asyns function
 
@@ -60,6 +65,54 @@ export const deleteCart = (courseId) => {
       await response.data;
     } catch (error) {
       errorHandler(error, dispatch);
+    }
+  };
+};
+
+// edit user details asyns function
+
+export const editUser = (
+  firstName,
+  lastName,
+  phoneNumber,
+  description,
+  address,
+  coverPhotoFile
+) => {
+  return async (dispatch) => {
+    try {
+      const ref = authService.storage.ref(`/userCoverImages/${coverPhotoFile.name}`);
+      const uploadTask = ref.put(coverPhotoFile);
+      uploadTask.on("state_changed", console.log, console.error, () => {
+        ref.getDownloadURL().then((url) => {
+          dispatch(updateUserDetails(firstName, lastName, phoneNumber, description, address, url));
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const updateUserDetails = (firstName, lastName, phoneNumber, description, address, url) => {
+  return async (dispatch) => {
+    try {
+      const addUserDetails = {
+        wishlist: [],
+        first_Name: firstName,
+        last_Name: lastName,
+        phone: phoneNumber,
+        description: description,
+        address: address,
+        coverPhotoUrl: url,
+      };
+      const response = await axios.put(UPDATE_USER_ENDPOINT, addUserDetails);
+      if (response.status === 200) {
+        const result = await response.data;
+        console.log(result);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 };
